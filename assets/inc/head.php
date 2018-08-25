5cdfb79ac0c99051d2bb816c66d0b2758306fb49
unknown <g.benske@field-interactive.com>
2018-08-25 18:55:46 +0200

    add sass files and headline fonts

--- assets/inc/head.php
+++ assets/inc/head.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
     <head>
         <meta charset="utf-8">
-        <link href="css/default.css" rel="stylesheet">
+        <link href="../css/default.css" rel="stylesheet">
 		<link rel="icon" type="image/png" href="img/favicon.png">
 		<title>Tabak Review GB</title>
     </head>
