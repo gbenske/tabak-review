0ec772a5916f3db7a1fd6da2c65f2c576cb73b5f
unknown <g.benske@field-interactive.com>
2018-11-16 21:47:31 +0100

    Programm sql instructions to show every flavor of the tobacco

--- pages/index.php
+++ pages/index.php
@@ -13,30 +13,16 @@
 ?>
 
  <?php 
-	// $sql = "SELECT s.Name, g.Geschmacksrichtung AS g1, g2.Geschmacksrichtung AS g2, g3.Geschmacksrichtung AS g3
-	//  		FROM sorte s 
-	//  		LEFT JOIN geschmack g ON s.ID_Geschmack = g.ID_Geschmack 
-	//  		LEFT JOIN geschmack g2 ON s.ID_Geschmack_2 = g2.ID_Geschmack
-	//  		LEFT JOIN geschmack g3 ON s.ID_Geschmack_3 = g3.ID_Geschmack";
-
- 	/*$sql = "SELECT s.Name as Tabaksorte, h.Name as Hersteller
-	 		FROM sorte s
-			LEFT JOIN hersteller h ON s.ID_Hersteller = h.ID_Hersteller"; */
-
-	//$sql = "SELECT *
-	//		FROM sorte s
-	//		LEFT JOIN geschmack_zu_sorte gs ON s.ID_Sorte = gs.ID_Sorte 
-	//		WHERE gs.ID_Sorte = 1";
-
-	$sql = "SELECT t.name AS tobacco, f.name AS flavor, m.name  AS manufacturer, r.count AS `count`, r.note AS note
+	$sql = "SELECT t.name AS tobacco, t.id AS tobaccoId, m.name  AS manufacturer, r.count AS `count`, r.note AS note
 			FROM tobacco t
 			INNER JOIN tobacco_to_flavor tf ON tf.id_tobacco = t.id
-			INNER JOIN flavor f ON f.id = tf.id_flavor
 			INNER JOIN manufacturer m ON m.id = t.id_manufacturer
-			INNER JOIN rating r ON r.id_tobacco = t.id";
+			INNER JOIN rating r ON r.id_tobacco = t.id
+			GROUP BY t.id";
 
 	$statement = $pdo->query($sql);
 	$tobaccos = $statement ->fetchALL();
+
 ?>
 
 	<div class="wrapper">
@@ -51,11 +37,25 @@
 		<?php } */?>
 
 
-		<?php foreach ($tobaccos as $tobacco) { ?>
+		<?php foreach ($tobaccos as $tobacco) { 
+
+			$sql = "SELECT f.name AS flavor
+					FROM tobacco_to_flavor tf
+					INNER JOIN flavor f ON f.id = tf.id_flavor
+					WHERE tf.id_tobacco = ".$tobacco['tobaccoId'];
+
+			$statement = $pdo->query($sql);
+			$flavors = $statement->fetchALL(PDO::FETCH_ASSOC);
+
+			$result = array_map(function($value) {
+				return $value['flavor'];
+			}, $flavors);
+
+			?>
 			<div class="col">
 				<h2><?php echo $tobacco['manufacturer'] ?></h2>
 				<div class="tobacco"><?php echo $tobacco['tobacco'] ?></div>
-				<div class="flavor"><?php echo $tobacco['flavor'] ?></div>
+				<div class="flavor"><?php echo implode(', ', $result) ?></div>
 				<div class="rating"><?php echo $tobacco['count'] ?></div>
 			</div>
 		<?php } ?>
