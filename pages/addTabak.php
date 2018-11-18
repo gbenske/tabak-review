fe6aa0de2467cfb781fdcd31a5549988cc8aac72
unknown <g.benske@field-interactive.com>
2018-11-18 12:19:27 +0100

    Remove every code snippet. Beginn with new Code

--- pages/addTabak.php
+++ pages/addTabak.php
@@ -4,11 +4,31 @@
     include_once $_SERVER['DOCUMENT_ROOT'] . "/assets/inc/header.php";
 ?>
 
+
 <?php 
 
+    // Hole mir alle Hersteller
     $statement = $pdo->prepare("SELECT * FROM manufacturer");
     $statement->execute();
-    $tabakManufacturer = $statement->fetchALL();
+    $tobaccoManufacturer = $statement->fetchALL();
+
+?>
+
+<?php 
+
+    // Hole mir alle Tabaksorten
+    $statement = $pdo->prepare("SELECT * FROM tobacco");
+    $statement->execute();
+    $tobaccoSort = $statement->fetchALL();
+
+?>
+
+<?php 
+
+    // Hole mir alle Tabaksorten
+    $statement = $pdo->prepare("SELECT * FROM flavor");
+    $statement->execute();
+    $flavorTobacco = $statement->fetchALL();
 
 ?>
 
@@ -16,11 +36,11 @@
  
     if(isset($_GET['rating'])) {
     $error = false;
-    $manufacturer = $_POST['manufacturer'];
     $addManufacturer = $_POST['addManufacturer'];
-    $tobacco = $_POST['tobacco'];
-    $flavor = $_POST['flavor'];
-    $rating = $_POST['bewertung'];
+    //$manufacturer = $_POST['manufacturer'];
+    //$tobacco = $_POST['tobacco'];
+    //$flavor = $_POST['flavor'];
+    //$rating = $_POST['bewertung'];
 
     // $folder = "../img/";
     // $image = $_FILES['image']['name'];
@@ -30,111 +50,9 @@
     // $allowed=array('jpeg','png' ,'jpg'); 
     // $filename=$_FILES['image']['name']; 
 
-    //Überprüfe, ob Tabaksorte schon vorhanden ist
-    if(!$error) { 
-             $statement = $pdo->prepare("SELECT * FROM tobacco WHERE name = :name");
-             $result = $statement->execute(array('name' => $tobacco));
-             $tabaktobacco = $statement->fetch();
-        
-         if($tabaktobacco !== false) {
-                 echo 'Dieser Tabaksorte ist bereits gespeichert!';
-             $error = true;
-          } else {
-
-            // Schreibe Tabaksorte zu entsprechenden Hersteller
-             if(isset($_POST['manufacturer'])) {
-                 $statement = $pdo->prepare("INSERT INTO tobacco (name, id_manufacturer) VALUES (:name, :id_manufacturer)");
-                 $result = $statement->execute(array('name' => $tobacco, 'id_manufacturer' => $manufacturer));
-
-                 if($result) { 
-                     echo 'Tabaksorte wurde hinzufügt.';
-                 } else {
-                     echo 'Beim Abspeichern ist leider ein Fehler aufgetreten';
-                 }
-             }
-
-            if($manufacturer == '') {
-                if (!empty($_POST['addManufacturer'])) {
-                    
-                    //Überprüfe, ob Hersteller schon vorhanden ist
-                      if(!$error) { 
-                            $statement = $pdo->prepare("SELECT * FROM manufacturer WHERE name = :name");
-                            $result = $statement->execute(array('name' => $addManufacturer));
-                            $tabakManufacturer = $statement->fetch();
-                         
-                            if($tabakManufacturer !== false) {
-                                    echo 'Dieser Hersteller ist bereits gespeichert!';
-                                $error = true;
-                             }  else {
-                                $statement = $pdo->prepare("INSERT INTO manufacturer (name) VALUES (:name)");
-                                $result = $statement->execute(array('name' => $addManufacturer));
-
-                               if (isset($_POST['addManufacturer'])) {
-                                    if($result) { 
-                                        echo 'Hersteller wurde hinzufügt.';
-                                    } else {
-                                        echo 'Beim Abspeichern ist leider ein Fehler aufgetreten';
-                                    }
-                               }
-                             }
-                         }
-                    }
-                }
-            }
-        }
-
-        if (!$error) {
-            $statement = $pdo->prepare("SELECT * FROM flavor WHERE name = :name");
-            $result = $statement->execute(array('name' => $flavor));
-            $flavortobacco = $statement->fetch();
-        
-         if($flavortobacco !== false) {
-                 echo 'Dieser Geschmack ist bereits gespeichert!';
-             $error = true;
-          } else {
-
-            // Schreibe Tabaksorte zu entsprechenden Hersteller
-             if(isset($_POST['flavor'])) {
-                 $statement = $pdo->prepare("INSERT INTO flavor (name) VALUES (:name)");
-                 $result = $statement->execute(array('name' => $flavor));
-
-                 if($result) { 
-                     echo 'Geschmack wurde hinzufügt.';
-                 } else {
-                     echo 'Beim Abspeichern ist leider ein Fehler aufgetreten';
-                 }
-             }
-         }
-        }
-
-    /*
-    //Keine Fehler, Bild und Tabak kann eingefügt werden
-    if(!$error) { 
-
-        $ext=pathinfo($filename, PATHINFO_EXTENSION);
-
-        if(!in_array($ext,$allowed)) { 
-
-            echo "Sorry, nur JPG, JPEG, PNG Datein sind erlaubt.";
-
-        } else { 
-            move_uploaded_file($_FILES['image'] ['tmp_name'], $path);
-            $statementImage=$pdo->prepare("INSERT INTO images (images) VALUES (:image)"); 
-            $statementImage->bindParam(':image',$image); 
-            $statementImage->execute(); 
-
-            echo "Bild wurde hochgeladen";
-        } 
-
-        $statement = $pdo->prepare("INSERT INTO tabak (manufacturer, tobacco, geschmack, bewertung) VALUES (:manufacturer, :tobacco, :geschmack, :bewertung)");
-        $result = $statement->execute(array('manufacturer' => $manufacturer, 'tobacco' => $tobacco, 'geschmack' => $taste, 'bewertung' => $rating));
- 
-        if($result) { 
-           echo 'Tabak wurde hinzufügt.';
-        } else {
-            echo 'Beim Abspeichern ist leider ein Fehler aufgetreten';
-        }
-    }  */
+    if(!$error) {
+        include_once('inc/addManufacturer.php');
+    }
 }
 ?>
 
@@ -143,19 +61,17 @@
 
      <form action="?rating" method="post" enctype="multipart/form-data">
 
-        <a href="javascript:void(0)" id="add_input_manufacturer">Neuen Hersteller hinzufügen</a>
-        <br/>
-        <br/>
-
-        <div id="manufacturer">
+        <div>
             <h2>Hersteller hinzufügen</h2>
             <input type="text" name="addManufacturer" />
             <input type="submit" value="Hersteller hinzufügen" />
        </div>
 
+       <?php /*
+
        <hr>
        
-        <h2>Hersteller</h2>
+        <h2>Hersteller auswählen</h2>
          <select name="manufacturer">
             <option disabled selected value=""> -- Wähle einen Hersteller aus -- </option>
             <?php foreach ($tabakManufacturer as $manufacturer): ?>
@@ -163,10 +79,6 @@
             <?php endforeach ?>
         </select>
         <br/><br/>
-
-        
-
-
 
         <h2>Tabaksorten</h2>
         <input type="text" name="tobacco" />
@@ -178,18 +90,40 @@
         <input type="text" name="flavor" />
         <input type="submit" value="Geschmack hinzufügen" />
 
-        <?php /*
+        <hr>
+
+        <h2>Tabaksorte auswählen</h2>
+         <select name="tobacco">
+            <option disabled selected value=""> -- Wähle einen Tabaksorte aus -- </option>
+            <?php foreach ($tobaccoSort as $tobacco): ?>
+                 <option value="<?= $tobacco['id']; ?>"><?= $tobacco['name']; ?></option>
+            <?php endforeach ?>
+        </select>
+        <br/><br/>
+
+        <h2>Geschmack der Tabaksorte hinzufügen</h2>
+         <select name="flavor">
+            <option disabled selected value=""> -- Wähle einen Tabaksorte aus -- </option>
+            <?php foreach ($flavorTobacco as $flavor): ?>
+                 <option value="<?= $flavor['id']; ?>"><?= $flavor['name']; ?></option>
+            <?php endforeach ?>
+        </select>
+        <input type="submit" value="Geschmack der Tabaksorte hinzufügen" />
+
+        
 
         <label>Bewertung</label>
         <input type="text" name="bewertung" />
 
          <input type="file" name="image" />
 
+         <input type="submit" name="addTobacco" value="Bewerten" />
+
         */
 
         ?>
 
-        <input type="submit" name="addTobacco" value="Bewerten" />
+        
     </form>
 </div>
 
