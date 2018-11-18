df27cada2f85438cf3f52ff75d60e8f0d03622b4
unknown <g.benske@field-interactive.com>
2018-11-18 21:40:27 +0100

    Add Google Fonts

--- assets/inc/head.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-    <head>
-        <meta charset="utf-8">
-        <link href="../assets/css/default.css" rel="stylesheet">
-		<link rel="icon" type="image/png" href="img/favicon.png">
-		<title>Tabak Review GB</title>
-    </head>
