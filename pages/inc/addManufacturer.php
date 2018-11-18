1b5e56aa1ca56f8f10c7571c72b46f91878dbf2c
unknown <g.benske@field-interactive.com>
2018-11-18 12:35:49 +0100

    Outsource code for add Tabak

---
+++ pages/inc/addManufacturer.php
@@ -0,0 +1,29 @@
+<?php
+
+    if($chooseManufacturer == '') {
+        if (!empty($_POST['addManufacturer'])) {
+            //Überprüfe, ob Hersteller schon vorhanden ist
+            if(!$error) { 
+                $statement = $pdo->prepare("SELECT * FROM manufacturer WHERE name = :name");
+                $result = $statement->execute(array('name' => $addManufacturer));
+                $tobaccoManufacturer = $statement->fetch();
+                
+                if($tobaccoManufacturer !== false) {
+                        echo 'Dieser Hersteller ist bereits gespeichert!';
+                    $error = true;
+                }  else {
+                    $statement = $pdo->prepare("INSERT INTO manufacturer (name) VALUES (:name)");
+                    $result = $statement->execute(array('name' => $addManufacturer));
+
+                    if (isset($_POST['addManufacturer'])) {
+                        if($result) { 
+                            echo 'Hersteller wurde hinzufügt.';
+                        } else {
+                            echo 'Beim Abspeichern ist leider ein Fehler aufgetreten';
+                        }
+                    }
+                }
+            }
+        }
+    }
+?>
---
+++ pages/inc/addTobacco.php
@@ -0,0 +1,25 @@
+<?php 
+    if(!$error) { 
+        $statement = $pdo->prepare("SELECT * FROM tobacco WHERE name = :name");
+        $result = $statement->execute(array('name' => $addTobacco));
+        $tobaccoSort = $statement->fetch();
+   
+    if($tobaccoSort !== false) {
+            echo 'Dieser Tabaksorte ist bereits gespeichert!';
+        $error = true;
+     } else {
+
+       // Schreibe Tabaksorte zu entsprechenden Hersteller
+        if(isset($_POST['chooseManufacturer'])) {
+            $statement = $pdo->prepare("INSERT INTO tobacco (name, id_manufacturer) VALUES (:name, :id_manufacturer)");
+            $result = $statement->execute(array('name' => $addTobacco, 'id_manufacturer' => $chooseManufacturer));
+
+            if($result) { 
+                echo 'Tabaksorte wurde hinzufügt.';
+            } else {
+                echo 'Beim Abspeichern ist leider ein Fehler aufgetreten';
+            }
+        }
+    }
+}
+?>
