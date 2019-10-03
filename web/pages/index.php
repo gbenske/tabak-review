4604274e12abf7961e7ec61ec5a7a6bc0d5c6907
gbenske <g.benske@field-interactive.com>
2019-10-03 11:23:47 +0100

    Change html structure

--- web/pages/index.php
+++ web/pages/index.php
@@ -69,7 +69,7 @@
 				<div class="col-60">
 					<div class="row flex">
 						<div class="col col-36">
-							<h2>Die besten Wasserpfeifen</h2>
+							<h2>Die besten <br/> Wasserpfeifen</h2>
 
 							<p>
 								Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
